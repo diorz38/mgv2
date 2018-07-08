ba4d75f1ac404b28cfda295f37a75ecef103f89d
diorz38 <dior@bps.go.id>
2018-07-09 01:10:02 +0700

    Deleting Unused Assets

--- public/modules/clip/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/pedro-castro/pen/KzGEQL.
-
- https://developer.mozilla.org/pt-BR/docs/Web/CSS/clip
--- public/modules/quadgallery/js/index.js
+++
@@ -1,4 +0,0 @@
-var quadimages = document.querySelectorAll("#quad figure");
-for(i=0; i<quadimages.length; i++) {
-  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); this.classList.toggle("full") }); 
-}
